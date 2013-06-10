704911a9707deef22d3eb7f8660ded504ace242a
sophietk <sophie.tk@gmail.com>
2013-06-11 00:31:33 +0200

    Geolocation from html to js view

--- moovsimply/src/main/webapp/js/GMapView.js
+++ moovsimply/src/main/webapp/js/GMapView.js
@@ -4,6 +4,7 @@ var GMapView = Backbone.View.extend({
     currentMarkers: [],
 
     render: function () {
+        this.geolocalize();
         var mapOptions = {
             zoom: 14,
             center: new google.maps.LatLng(48.87525, 2.31110),
@@ -12,5 +13,36 @@ var GMapView = Backbone.View.extend({
         this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
         return this;
+    },
+
+    geolocalize: function () {
+        if (navigator.geolocation) {
+            var watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, {enableHighAccuracy: true, timeout: 10000, maximumAge: 600000});
+        }
+        else {
+            alert("pas de géolocalisation HTML5 possible avec ce navigateur");
+        }
+
+        function successCallback(position) {
+            alert("Votre position - Latitude : " + position.coords.latitude + ", longitude : " + position.coords.longitude);
+        }
+
+        function errorCallback(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    alert("L'utilisateur n'a pas autorisé l'accès à sa position");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert("Votre position n'a pas pu être déterminée");
+                    break;
+                case error.TIMEOUT:
+                    alert("timeout geo html5");
+                    break;
+            }
+        }
+    },
+
+    stopWatch: function () {
+        navigator.geolocation.clearWatch(watchId);
     }
 });
