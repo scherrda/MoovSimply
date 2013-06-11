16cf62bdf2cdb094f1187585121c9d22475e491e
ellene.dijoux@gmail.com <ellene.dijoux@gmail.com>
2013-06-11 14:00:16 +0200

    Init configurations for Heroku

--- moovsimply/src/main/java/fr/duchesses/App.java
+++ moovsimply/src/main/java/fr/duchesses/App.java
@@ -1,13 +1,34 @@
 package fr.duchesses;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java!\n");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new App()),"/*");
+        server.start();
+        server.join();
     }
 }
