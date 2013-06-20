a7250c3b016867ff8ce8791ac1f707306019196d
sophietk <sophie.tk@gmail.com>
2013-06-20 23:10:34 +0200

    Last lines icons

--- src/main/webapp/js/SplashView.js
+++ src/main/webapp/js/SplashView.js
@@ -1,5 +1,7 @@
 var SplashView = Backbone.View.extend({
 
+    className: 'splash withoutTopBar',
+
     render: function () {
         this.$el.html($('#splash-tmpl').html());
         return this;
